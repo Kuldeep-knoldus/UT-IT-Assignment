58d25076a4719a00df232d4462cbb582c5dcb945
Kuldeep <76957624+Kuldeep-knoldus@users.noreply.github.com>
2021-02-14 00:19:07 +0530

    Assignment Completed

--- UT_IT_Assignment/Ques_3/src/test/scala/com/knoldus/validator/CompanyValidatorTest.scala
+++ UT_IT_Assignment/Ques_3/src/test/scala/com/knoldus/validator/CompanyValidatorTest.scala
@@ -8,14 +8,14 @@ class CompanyValidatorTest extends AnyFlatSpec {
     val companyValidator = new CompanyValidator()
     val userInputCompany: Company = Company("Company_A","Company_A@comp.com", "USA")
     val result = companyValidator.companyIsValid(userInputCompany)
-    assert(!result)
+    assert(result)
   }
 
   it should "valid because it is contains valid company" in {
     val companyValidator = new CompanyValidator()
     val userInputCompany: Company = Company("Knoldus", "knoldus@knoldus.com", "Noida")
     val result = companyValidator.companyIsValid(userInputCompany)
-    assert(result)
+    assert(!result)
   }
 
 }
